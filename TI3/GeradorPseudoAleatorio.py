cdc198fcc09660a6e9a9ea95fbb18631cc7e0393
arthur <arthurjolo@gmail.com>
2024-05-24 11:03:23 -0300

    implementação do lagged fibonacci algorithm
    Signed-off-by: arthur <arthurjolo@gmail.com>

--- TI3/GeradorPseudoAleatorio.py
+++ TI3/GeradorPseudoAleatorio.py
@@ -6,10 +6,18 @@ class GeradorPseudoAleatório():
         self.lcg_m = 19993
         self.lcg_a = 15005
         self.lcg_b = 8371
+        self.lcg_seed = 1
+        ################
+        # Definição dos parametros para o LFG
+        self.lfg_l = 17
+        self.lfg_k = 5
+        self.lfg_m = 2**31
+        self.lfg_seed = self.lfg_l #start poin, muda para não gerar o mesmo número
+        self.lagged_fib_values = [0, 1]
+        self.init_lagged_fib()
         ################
         self.tamanho = tamanho
         self.algoritimo = algoritimo
-        self.seed = 1
     
     def set_tamanho(self, tamanho):
         self.tamanho = tamanho
@@ -25,14 +33,36 @@ class GeradorPseudoAleatório():
     
     def gerar_numero(self):
         if(self.algoritimo == "LCG"):
-            
-            return self.lcg()
+            return int(self.lcg(),2)
+        elif(self.algoritimo == "LFG"):
+            return int(self.lfg(),2)
     
+    def fibonacci(self, count):
+        for i in range(2,count):
+            new_fib = self.lagged_fib_values[i - 1] + self.lagged_fib_values[i - 2] % self.lfg_m
+            #print(i)
+            self.lagged_fib_values.append(new_fib)
+        return new_fib
+    def init_lagged_fib(self):
+        self.lagged_fib_values = [0,1]
+        self.fibonacci(self.lfg_l)
+
+    def lfg(self):
+        binary = ""
+        for i in range(self.tamanho):
+            si = (self.lagged_fib_values[self.lfg_seed + i - self.lfg_l] +
+                  self.lagged_fib_values[self.lfg_seed + i - self.lfg_k]) % self.lfg_m
+            zi = si % 2
+            # Concatena o bit gerado ao binário aleatório a ser retornado
+            binary += str(zi)
+            self.lagged_fib_values.append(si)
+        self.lfg_seed += 1
+        return binary
     # Algoritimo LCG
     def lcg(self):
         binary = ""
         # s0 = semente
-        s = self.seed
+        s = self.lcg_seed
         # Loop de geração dos cada bit para uma cadeia com o tamanho escolhido
         for i in range(self.tamanho):
             # LCG para gerar um número pseudo aleatório
@@ -45,7 +75,7 @@ class GeradorPseudoAleatório():
             s = si
         # Atualiza a semente para que na próxima chamada do algoritimo
         #seja gerado um número diferente
-        self.seed += 1
+        self.lcg_seed += 1
         return binary
 
 tamanhos = [40, 56, 80, 128, 168, 224, 256, 512, 1024, 2048, 4096]
@@ -60,4 +90,15 @@ for tamanho in tamanhos:
     elapsed_time = end_time - start_time
     elapsed_times_lcg.append(elapsed_time)
 
+elapsed_times_lfg = []
+gerador.set_algoritimo("LFG")
+for tamanho in tamanhos:
+    start_time = time.time()
+    gerador.set_tamanho(tamanho)
+    for i in range(100):
+        gerador.gerar_numero()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    elapsed_times_lfg.append(elapsed_time)
 
+print(elapsed_times_lfg)
